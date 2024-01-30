2e2b2cc60ab27924883d8cf69066d5a83e53d45e
chris_twkim <twkim326@gmail.com>
2024-01-30 23:04:20 +0900

    coupon Prefix 제거

--- src/main/java/kr/co/ooweat/coupon/api/IssuanceController.java
+++ src/main/java/kr/co/ooweat/coupon/api/IssuanceController.java
@@ -1,16 +1,18 @@
 package kr.co.ooweat.coupon.api;
 
 import kr.co.ooweat.coupon.application.IssuanceService;
-import kr.co.ooweat.coupon.application.dto.ConfigRequest;
 import kr.co.ooweat.coupon.application.dto.IssuanceRequest;
 import kr.co.ooweat.coupon.application.dto.IssuanceResponse;
+import kr.co.ooweat.coupon.application.dto.StatusResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/coupon/issuance")
+@RequestMapping("/api/v1/issuance")
 public class IssuanceController {
     
     private IssuanceService issuanceService;
@@ -19,15 +21,28 @@ public class IssuanceController {
         this.issuanceService = issuanceService;
     }
     
-    //TODO: Coupon 기본값에 대한 interceptor 처리 진행할 예정
+    //TODO: Coupon 기본값에 대한 interceptor 처리 진행할 예정 Swagger 로 처리할지 고민
     @PostMapping
     public ResponseEntity<IssuanceResponse> issuance(final IssuanceRequest issuanceRequest) {
         try {
-            IssuanceResponse response = issuanceService.save(issuanceRequest);
+            IssuanceResponse response = issuanceService.issuance(issuanceRequest);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
         }
     }
+    
+    @PatchMapping("/resend/coupon/{couponNo}}")
+    public ResponseEntity<IssuanceResponse> resendByCouponNo(@PathVariable("couponNo") final String couponNo){
+        try {
+            IssuanceResponse response = issuanceService.reSendByCouponNo(couponNo);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
+
 }
--- src/main/java/kr/co/ooweat/coupon/api/StatusController.java
+++ src/main/java/kr/co/ooweat/coupon/api/StatusController.java
@@ -1,2 +1,31 @@
-package kr.co.ooweat.coupon.api;public class StatusController {
+package kr.co.ooweat.coupon.api;
+
+import kr.co.ooweat.coupon.application.StatusService;
+import kr.co.ooweat.coupon.application.dto.StatusResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/status")
+public class StatusController {
+    private StatusService statusService;
+    
+    public StatusController(final StatusService statusService) {
+        this.statusService = statusService;
+    }
+    
+    @PatchMapping("/{status}/coupon/{couponNo}}")
+    public ResponseEntity<StatusResponse> updateCouponStatus(@PathVariable("status") final char status, @PathVariable("couponNo") final String couponNo){
+        try {
+            StatusResponse response = statusService.updateCouponStatus(status, couponNo);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
 }
